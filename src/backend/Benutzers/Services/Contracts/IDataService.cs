5abbd0690a99f3e4558cbea801597d6433d11194
Timboo22 <t.lutty@t-online.de>
2026-02-11 18:07:16 +0100

    DBService + neue Unit Test

---
+++ src/backend/Benutzers/Services/Contracts/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace SanoaAPI.Benutzers.Services.Contracts;
+
+public interface IDataService
+{
+    IQueryable<T> GetAll<T>() where T : class;
+    
+    void Add<T>(T entity) where T : class;
+    
+    void Remove<T>(T entity) where T : class;
+    
+    void Save();
+}
---
+++ src/backend/Benutzers/Services/DataService.cs
@@ -0,0 +1,19 @@
+﻿namespace SanoaAPI.Benutzers.Services.Contracts;
+
+public class DataService : IDataService
+{
+    private readonly ContextDb _context;
+
+    public DataService(ContextDb context)
+    {
+        _context = context;
+    }
+    
+    public void Add<T>(T entity) where T : class => _context.Add(entity);
+    
+    public IQueryable<T> GetAll<T>() where T : class => _context.Set<T>();
+    
+    public void Remove<T>(T entity) where T : class => _context.Remove(entity);
+    
+    public void Save() => _context.SaveChanges();
+}
