478a2f8e7400af28ede6dfad1b6b80614c45f1d4
timboo22 <t.lutty@t-online.de>
2026-02-20 21:39:49 +0100

    Fixes ZwichenCommit

--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {Home} from './home/home';
-import {NgOptimizedImage} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 import {Navbar} from './navbar/navbar';
 import {Card} from 'primeng/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, NgOptimizedImage, RouterLink, Navbar, Card],
+  imports: [RouterOutlet, Navbar, Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
